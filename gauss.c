11f517269c4bfb3aa6975209134365fee2784cca
Jorix1 <pawelostaszewski0@gmail.com>
2025-12-19 15:08:03 +0100

    Gauss eliminacja v2
    Poprawiona wersja eliminacji gaussa bez gauss test i z sprawdzaniem czy jest ona osobliwa i kwadratowa

--- gauss.c
+++ gauss.c
@@ -2,47 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
+// porownanie kolumn i rzedow
+
+
 int eliminate(Matrix *mat, Matrix *b){
 	int liczbaR = b->r;
 	int liczbaC = b->c;
-	double **matryca = b->data;
-	double **mat_wyn = mat->data;
-    // if( obliczanieWyz == 1) return 1;
+	
+
+	if(liczbaC!=liczbaR) return 0;
+
 	int i ,j,k;
 	double ratio;
 	for(i=0;i<liczbaC;i++){
-		for(j=0;j<liczbaR;j++){
+		for(j=i;j<liczbaR;j++){
 			if(j>i){
-				ratio = matryca[j][i]/matryca[i][i];
+				ratio = b->data[j][i]/b->data[i][i];
 				for(k=0;k<liczbaC;k++){
-					matryca[j][k] = matryca[j][k]-ratio *matryca[i][k];
+					b->data[j][k] = b->data[j][k]-ratio *b->data[i][k];
 					
 				}
-				mat_wyn[i][0] = mat_wyn[i][0]-ratio*mat_wyn[i][0];
+				mat->data[j][0] = mat->data[j][0]-ratio*mat->data[i][0];
+				
 			}
 		}
+		if(b->data[i][i]==0) return 0;
 	}
-		
+	
+	return 0;	
 }	
-
-// int det = 0;
-// int obliczanieWyz(Matrix *b){
-// 	int liczbaR = b->r;
-// 	int liczbaC = b->c;
-// 	int wyznacznik = 0;
-// 	int **matryca = b->data;
-// 	int liczba0 = 0;
-
-// 	if(liczbaC!=liczbaR) return 0;// sprawdza czy macierz jest kwadratowa
-
-// 	else{//sprawdza czy wiersz nie składa się z 0
-		
-		
-// 	}
-// 	return 1;
-// }
 
